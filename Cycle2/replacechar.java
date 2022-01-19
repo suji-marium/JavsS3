6547963f5e909e5a10eeee3f782418b23c826462
suji-marium <85631667+suji-marium@users.noreply.github.com>
2022-01-20 00:06:05 +0530

    Create replacechar.java
    Program to replace a particular character in a string.

---
+++ Cycle2/replacechar.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+public class Main
+{
+    public static void main(String args[])
+    {
+        int i;
+        Scanner obj = new Scanner(System.in);
+        System.out.print("Enter the string length ");
+        int n = obj.nextInt();
+        System.out.println("Enter string elements ");
+        char a[] = new char[20];
+        
+        for(i=1;i<=n;i++)
+            a[i] = obj.next().charAt(0);
+        
+        System.out.println("Enter the position of the character to be replaced :");
+        int x = obj.nextInt();
+        
+        System.out.println("Enter the character to be placed at previous position:");
+        char ch = obj.next().charAt(0);
+        
+        for(i=1;i<=n;i++)
+             a[x] = ch;
+        System.out.println("Replaced String");
+        for(i=1;i<=n;i++)
+            System.out.println(a[i]);
+    }
+}
